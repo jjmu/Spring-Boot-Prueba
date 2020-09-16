6719a905a37e3ba903c932155252f7546ff6b5ac
tech <tech@Hideyoshi>
2020-09-16 00:28:29 -0500

    Practica de dia 4. User y role creado para el repositorio, con servicio para users y lista de usuarios y roles en la vista.

---
+++ inventario/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+
+	//public Optional findByUsername(String username);
+	
+//	public Optional findByIdAndPassword(Long id, String password);
+
+}
